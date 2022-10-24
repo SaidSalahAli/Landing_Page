012632c38af2f97ffa8f20d8827af99479a34dde
SaidSalahAli <saidsalah375@gmail.com>
2022-10-24 10:17:15 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -10,59 +10,60 @@
 const btn = document.getElementById("btn");
 const ul = document.getElementById("navbar__list");
 const header = document.querySelector(".page__header");
+const sections = Array.from(document.querySelectorAll("section"));
 
 /**
  * class Template page
- */
-class Template {
-  counter = 0;
-  /**
-   * Add new Section
-   * @returns Html
-   */
-  addNewSection() {
-    return `<section id="section${this.counter}" data-nav="Section ${this.counter}">
-    <div class="landing__container">
-      <h2>Section ${this.counter}</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-          fermentum metus faucibus lectus pharetra dapibus. Suspendisse
-          potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget
-          lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed
-          convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla
-          eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam
-          nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis
-          lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a
-          tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus
-          vitae elit. Integer nec libero venenatis libero ultricies molestie
-          semper in tellus. Sed congue et odio sed euismod.
-        </p>
+//  */
+// class Template {
+//   counter = 0;
+//   /**
+//    * Add new Section
+//    * @returns Html
+//    */
+//   addNewSection() {
+//     return `<section id="section${this.counter}" data-nav="Section ${this.counter}">
+//     <div class="landing__container">
+//       <h2>Section ${this.counter}</h2>
+//         <p>
+//           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+//           fermentum metus faucibus lectus pharetra dapibus. Suspendisse
+//           potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget
+//           lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed
+//           convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla
+//           eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam
+//           nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis
+//           lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a
+//           tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus
+//           vitae elit. Integer nec libero venenatis libero ultricies molestie
+//           semper in tellus. Sed congue et odio sed euismod.
+//         </p>
 
-        <p>
-          Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar
-          gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam.
-          Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum
-          consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget
-          elementum tortor mollis non.
-        </p>
-      </div>
-    </div>
-  </section>`;
-  }
+//         <p>
+//           Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar
+//           gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam.
+//           Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum
+//           consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget
+//           elementum tortor mollis non.
+//         </p>
+//       </div>
+//     </div>
+//   </section>`;
+//   }
 
-  /**
-   * genrate template
-   */
-  genrate() {
-    this.counter++;
-    document
-      .querySelector("main")
-      .insertAdjacentHTML("beforeend", this.addNewSection());
-  }
-}
+//   /**
+//    * genrate template
+//    */
+//   genrate() {
+//     this.counter++;
+//     document
+//       .querySelector("main")
+//       .insertAdjacentHTML("beforeend", this.addNewSection());
+//   }
+//}
 
 // instance of Template
-const temp = new Template();
+// const temp = new Template();
 
 /**
  * start build the nav
@@ -133,22 +134,21 @@ class Navbar {
   }
 }
 
-
 let navbar = new Navbar();
 
 (function () {
   btn.addEventListener("click", () => {
-    temp.genrate();
+    // temp.genrate();
     navbar.menu();
   });
 
   /**
    * creating more sections after loded the page
-   */
-  addEventListener("load", () => {
-    for (let i = 0; i < 4; i++) {
-      temp.genrate();
-      navbar.menu();
-    }
-  });
+  //  */
+  // addEventListener("load", () => {
+  //   for (let i = 0; i < 4; i++) {
+  //     temp.genrate();
+  //     navbar.menu();
+  //   }
+  // });
 })();
