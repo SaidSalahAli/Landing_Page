cf7c558c9816b25bbcbba3518496e1c41bef8e62
SaidSalahAli <saidsalah375@gmail.com>
2022-10-28 09:54:14 +0200

    end Commit

--- js/app.js
+++ js/app.js
@@ -25,13 +25,11 @@ class Navbar {
   /**
    *	 Build menu
    */
-  menu() { 
-    
+  menu() {
     ul.innerHTML = "";
     sections.forEach((section) => {
       const listItem = `<li><a data-nav="${section.id}" class="menu__link">${section.dataset.nav}</a></li>`;
       ul.insertAdjacentHTML("beforeend", listItem);
-      console.log(section.id);
     });
   }
 
